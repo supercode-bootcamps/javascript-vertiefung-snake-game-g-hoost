742c896eb80a3e527c9f9fd171be00931d6d81c1
Georgia Hoost <georgia@Georgias-iMac.fritz.box>
2021-02-24 13:49:47 +0100

    divs done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 
 let div = document.createElement('div');
+
 div.innerHTML = '<div></div>';
 document.body.appendChild(div);
 div.id = ("snakeField");
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+    let newDiv = document.createElement('div');
+    snakeField.appendChild(newDiv);
+    newDiv.className = 'item';
+};
