8f724f1a392fbe19666f9d7ccdf3865de934ad79
Georgia Hoost <gfroehling@icloud.com>
2021-02-24 19:05:48 +0100

    game working, extra yellow div

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,22 @@ let div = document.createElement('div');
 div.innerHTML = '<div></div>';
 document.body.appendChild(div);
 div.id = ("snakeField");
+let color = 100;
 
-
-
-
-for (let i = 1; i <= 100; i++) {
+for (let i = 0; i <= 99; i++) {
     let newDiv = document.createElement('div');
     snakeField.appendChild(newDiv);
     newDiv.className = 'item';
+    newDiv.addEventListener('mouseover', (e) =>
+    {
+        e.target.style.backgroundColor = 'orange';
+        e.target.style.backgroundColor = 'yellow'[Math.floor(Math.random()(4000) * 'yellow')];
+        setTimeout(() =>
+        {
+            e.target.style.backgroundColor = 'lightblue';
+        }, 500);
+
+    });
+
+
 };
