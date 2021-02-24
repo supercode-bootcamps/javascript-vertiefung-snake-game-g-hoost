cadcab30c4fc50a031091d1c32e900b7ef956064
Georgia Hoost <gfroehling@icloud.com>
2021-02-24 21:01:27 +0100

    almost final

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ let div = document.createElement('div');
 div.innerHTML = '<div></div>';
 document.body.appendChild(div);
 div.id = ("snakeField");
-let color = 100;
+let score = 0;
 
 for (let i = 0; i <= 99; i++) {
     let newDiv = document.createElement('div');
@@ -13,13 +13,33 @@ for (let i = 0; i <= 99; i++) {
     newDiv.addEventListener('mouseover', (e) =>
     {
         e.target.style.backgroundColor = 'orange';
-        e.target.style.backgroundColor = 'yellow'[Math.floor(Math.random()(4000) * 'yellow')];
         setTimeout(() =>
         {
             e.target.style.backgroundColor = 'lightblue';
         }, 500);
-
     });
-
-
 };
+
+let snakeItem = document.getElementsByClassName('item');
+
+let foodItem = () =>
+{
+    let redDiv = 0;
+
+    setInterval(function ()
+    {
+        redDiv = Math.floor(Math.random() * 99);
+        snakeItem[redDiv].classList.add('food');
+        snakeItem[redDiv].addEventListener('mouseover', (e) =>
+        {
+            e.target.classList.remove('food');
+            let yourScore = document.createElement('p');
+            snakeField.appendChild(yourScore);
+            let scoreHtml = yourScore.innerHTML;
+            count = Number(score);
+            yourScore.innerHTML = 'your score is: ' + score++;
+            // yourScore = document.removeChild(yourScore);
+        });
+    }, 3000);
+};
+foodItem();
